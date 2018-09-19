c575dc3490240b1c2f0deeed8c49c74a6eb09496
manu <cibermanu@hotmail.com>
2018-09-19 22:50:54 +0200

    Saving progress:: Newsmodel: downloading news json and pictures

--- app/src/main/java/app/manu/whatsoncrypto/classes/news/news.kt
+++ app/src/main/java/app/manu/whatsoncrypto/classes/news/news.kt
@@ -7,45 +7,48 @@ class News {
     constructor(
             headline: String,
             body: String,
-            lead: String? = null,
+
             image: Bitmap? = null,
+            imageURL: String? = null,
             url: String? = null,
             timestamp: Long? = null) {
 
         this.headline = headline
-        this.lead = lead
+
         this.picture = image
+        this.imageURL = imageURL
         this.body = body
         this.url = url
         this.timestamp = timestamp
     }
 
-    private var headline: String
+    public var headline: String
         get() = this.headline
         set(value: String) {
             headline = value
         }
-    private var lead: String?
-        get() = this.lead
-        set(value: String?) {
-            lead = value
-        }
-    private var body: String
+
+    public var body: String
         get() = this.body
         set(value: String) {
             body = value
         }
-    private var picture: Bitmap?
+    public var picture: Bitmap?
         get() = this.picture
         set(value: Bitmap?) {
             picture = value
         }
-    private var url: String?
+    public var imageURL: String?
+        get() = this.imageURL
+        set(value: String?) {
+            imageURL = value
+        }
+    public var url: String?
         get() = this.url
         set(value: String?) {
             url = value
         }
-    private var timestamp: Long?
+    public var timestamp: Long?
         get() = this.timestamp
         set(value: Long?) {
             timestamp = value
--- app/src/main/java/app/manu/whatsoncrypto/models/NewsModel.kt
+++ app/src/main/java/app/manu/whatsoncrypto/models/NewsModel.kt
@@ -1,10 +1,13 @@
 package app.manu.whatsoncrypto.models
 
+import android.graphics.Bitmap
 import android.os.AsyncTask
 import app.manu.whatsoncrypto.classes.news.News
 import org.json.JSONObject
 import org.json.JSONArray
 import app.manu.whatsoncrypto.utils.JSON.JSONParser
+import android.graphics.BitmapFactory
+import android.util.Log
 
 
 class NewsModel {
@@ -76,23 +79,71 @@ class NewsModel {
     private fun jsonNewsAsNewsArray(json_query_result: JSONObject?) : List<News> {
         val data = json_query_result!!.get("Data") as JSONArray
         val result_list = mutableListOf<News>()
+        val imageUrl = Array<String?>(data.length(), {i-> null})
         for (i in 0 until data.length()) {
             val newsJSON_obj = data[i] as JSONObject
 
+            val published_timestamp = newsJSON_obj.get("published_on").toString().toLong()
+            val imageurl = newsJSON_obj.get("imageurl") as String
+            val headline = newsJSON_obj.get("title") as String
+            val url = newsJSON_obj.get("url") as String
+            val body = newsJSON_obj.get("body") as String
 
+            val news = News(headline,body,null, imageurl, url, published_timestamp)
 
-
-
-            val published_timestamp = newsJSON_obj.get("published_on") as String
-            val picture_url = newsJSON_obj.get("published_on") as String
-            val published_timestamp = newsJSON_obj.get("published_on") as String
-
-
-
-
-
+            imageUrl.set(i, imageurl)
 
         }
+
+        val f: (Any?) -> Unit = {
+            m ->
+                val mapa = m as Map<String?, Bitmap?>
+                for ((integer_key, url_key) in imageUrl.withIndex()) {
+                    val value = mapa.get(url_key)
+                    if (value != null) {
+                        for (news in result_list){
+                            if (news.imageURL == url_key) {
+                                news.picture = value
+                                break
+                            }
+                        }
+                    }
+                }
+        }
+
+        val onFinish: List<(Any?) -> Any?> = listOf(
+                f
+        )
+
+        downloadImages(imageUrl, onFinish)
         return result_list
+    }
+
+    private fun downloadImages(url_arr: Array<String?>, onFinish: List<(Any?) -> Any?>, limit: Int = 10) {
+        _mOnFinishAsyncMachineFunctions.clear()
+
+        _mOnFinishAsyncMachineFunctions.addAll( onFinish )
+
+        _mAsyncCode.clear()
+
+        val function_to_exec: (Array<out String?>) -> Any? = fun(param: Array<out String?>) : Any? {
+            val loop_limit = Math.min(url_arr.size, limit)
+            val result: MutableMap<String?, Bitmap?> = mutableMapOf()
+            for (i in 0 until loop_limit) {
+                val urldisplay = url_arr[i]
+                var mImage: Bitmap? = null
+                try {
+                    val `in` = java.net.URL(urldisplay).openStream()
+                    mImage = BitmapFactory.decodeStream(`in`)
+                } catch (e: Exception) {
+                    Log.e("Error", e.message)
+                    e.printStackTrace()
+                }
+                result.put(urldisplay, mImage)
+            }
+            return result
+        }
+        _mAsyncCode.add( function_to_exec )
+        _myAsyncMachine!!.execute()
     }
 }
